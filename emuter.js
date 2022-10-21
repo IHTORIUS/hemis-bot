879ea608d8f6735accd369d802046ac9f1bf8198
ihtorius <ihtorius@gmail.com>
2022-10-21 10:20:14 +0500

    Edit html file

--- emuter.js
+++ emuter.js
@@ -6,7 +6,7 @@ const url = 'https://student.fbtuit.uz/dashboard/login';
 currentdate = new Date();
 var oneJan = new Date(currentdate.getFullYear(), 0, 1);
 var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-var thisweek = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7) + 4126;
+var thisweek = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7) + 4125;
 
 
 async function getPage(){
